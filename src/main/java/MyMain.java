67f023a8c7352e7bb508801daf78f613910fb5ef
HEL3X <person_of_paradise@hotmail.com>
2021-09-03 13:57:04 -0700

    Median, Magnitude, and Pythagoras Methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,18 +4,36 @@ public class MyMain {
 
     // Calculates the median of the three inputs
     public static int median(int a, int b, int c) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (a<b && b<c){
+            return b;
+        }else if (a>b && b>c){
+            return b;
+        }else if (b<a && a<c){
+            return a;
+        }else if (b>a && a>c){
+            return a;
+        }else{
+            return c;
+        }
     }
 
     // Returns the input with the larger absolute value
     public static int magnitude(int a, int b) {
-        return 0; // REPLACE WITH YOUR CODE
+        if (java.lang.Math.abs(a) > java.lang.Math.abs(b)){
+            return a;
+        } else if (java.lang.Math.abs(b) > java.lang.Math.abs(a)){
+            return b;
+        }else{
+            return a;
+        }
     }
 
     // Returns the "c" value from the Pythagorean theorem "a^2 + b^2 = c^2",
     // where "a" and "b" are the inputs to the method
     public static double pythagoras(int a, int b) {
-        return 0.0; // REPLACE WITH YOUR CODE
+        double cc = a*a + b*b;
+        double c = Math.sqrt(cc);
+        return c;
     }
 
     public static void main(String[] args) {
